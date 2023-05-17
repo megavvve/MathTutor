db641a26f9ce8eaa243757b93be694ad7eec9948
megavvve <aserka59tom@gmail.com>
2023-05-17 11:40:58 +0300

    git init

---
+++ MathTutor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathTutor 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
