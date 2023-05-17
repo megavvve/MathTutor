80d9d295a5eddffb259a2adce32c29bbeb98a5ac
danilgordienko <79045430+danilgordienko@users.noreply.github.com>
2023-05-17 13:41:23 +0300

    add class Dot

---
+++ MathTutor/part A/Dot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTutor.part_A
+{
+    internal class Dot
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public Dot(double x, double y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
