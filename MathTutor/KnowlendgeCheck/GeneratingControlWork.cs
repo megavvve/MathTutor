33b58e116d3041a6f27df71d282a7724bb358201
danilgordienko <79045430+danilgordienko@users.noreply.github.com>
2023-06-02 14:21:05 +0300

    add questions with 4 answers

--- MathTutor/KnowlendgeCheck/GeneratingControlWork.cs
+++ MathTutor/KnowlendgeCheck/GeneratingControlWork.cs
@@ -24,24 +24,31 @@ namespace MathTutor.KnowlendgeCheck
         /// </summary>
         /// <param name="countTasks"></param>
         /// <returns></returns>
-        public List<(string question, string hint, double answer)> Generating(int countTasks, int countControlWork)
+        public List<(string question, string hint, List<double> answer)> Generating(int countTasks, int countControlWork)
         {
-            if (countTasks > Questions.Count)
+            var a = QuestionsWithFourAnswers.Count / countControlWork;
+            if (countTasks * countControlWork > Questions.Count + QuestionsWithFourAnswers.Count || a == 0 && countTasks * countControlWork > Questions.Count)
             {
                 Console.WriteLine("Недостаточно вопросов для генерации.");
-                return new List<(string question, string hint, double answer)>();
+                return new List<(string question, string hint, List<double> answer)>();
             }
-
-            List<(string question, string hint, double answer)> controlWorks = new List<(string question, string hint, double answer)>();
+            var controlWorks = new List<(string question, string hint, List<double> answer)>();
             Random rnd = new Random();
             while (countControlWork != 0)
             {
-                controlWorks.Add(($"                                                                                            Вариант номер {countControlWork}", string.Empty, 0));
-                for (int i = 0; i < countTasks; i++)
+                controlWorks.Add(($"                                           Вариант номер {countControlWork}", string.Empty, new List<double>()));
+                for (int i = 0; i < a; i++)
+                {
+                    controlWorks.Add((QuestionsWithFourAnswers[0], HintsForQuestionsWithFourAnswers[0], AnswersForQuestionsWithFourAnswers[0]));
+                    QuestionsWithFourAnswers.RemoveAt(0);
+                    HintsForQuestionsWithFourAnswers.RemoveAt(0);
+                    AnswersForQuestionsWithFourAnswers.RemoveAt(0);
+                }
+                for (int i = 0; i < countTasks - a; i++)
                 {
                     int r = rnd.Next(0, Questions.Count);
-                    controlWorks.Add((Questions[r], Hints[r], Answers[r]));
-
+                    var s = new List<double>() { Answers[r] };
+                    controlWorks.Add((Questions[r], Hints[r], new List<double>() { Answers[r]}));
                     Questions.RemoveAt(r);
                     Answers.RemoveAt(r);
                     Hints.RemoveAt(r);
@@ -71,7 +78,7 @@ namespace MathTutor.KnowlendgeCheck
         /// Создает файлы с рандомными задачами
         /// </summary>
         /// <param name="lists"></param>
-        public void GenerateFiles(List<(string question, string hint, double answer)> tasks)
+        public void GenerateFiles(List<(string question, string hint, List<double> answers)> tasks)
         {
             string fileName = "./ControlWork.txt";
             string file = "./Hints.txt";
@@ -80,6 +87,19 @@ namespace MathTutor.KnowlendgeCheck
                 foreach (var task in tasks)
                 {
                     writer.WriteLine(task.question);
+                    if (task.answers.Count >= 1)
+                    {
+                        if (task.answers.Count > 1)
+                        {
+                            var i = 1;
+                            foreach (var answer in task.answers)
+                            {
+                                writer.WriteLine($"{i}) {answer}");
+                                i++;
+                            }
+                        }
+                        writer.WriteLine($"Ответ - {task.answers[0]}");
+                    }
                 }
             }
             using (StreamWriter writer = new StreamWriter(file))
